a797813b468181aff6b2a4f92432b70a850f1836
Bagus Puji Rahardjo <bagus.rahardjo6@gmail.com>
2021-05-20 17:37:23 +0700

    Pengumpulan Tugas-4 Pekan 3

--- Laravel/database/migrations/2021_05_20_043602_create_genres_table.php
+++ Laravel/database/migrations/2021_05_20_043602_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 45);
-            $table->string('email', 45);
-            $table->string('password', 45);
+            $table->string("nama", 45);
         });
     }
 
@@ -28,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genres');
     }
 }
